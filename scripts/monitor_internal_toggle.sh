d36b3f3c2fa27680f354ff6ee62fb696f11fa263
reg1z <21206554+reg1z@users.noreply.github.com>
2025-04-14 13:28:57 -0700

    Script fix

--- scripts/monitor_internal_toggle.sh
+++ scripts/monitor_internal_toggle.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <monitor>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <monitor1> <monitor2>"
     exit 1
 fi
 
-MONITOR=$1
+MONITOR1=$1
+MONITOR2=$2
 
 # Get current monitor status
-STATUS=$(hyprctl monitors | grep "$MONITOR")
+STATUS=$(hyprctl monitors | grep "$MONITOR1")
 
 if [[ -n "$STATUS" ]]; then
     # Monitor is active, disable it
-    hyprctl keyword monitor "$MONITOR,disable"
+    hyprctl keyword monitor "$MONITOR1,disable"
 else
     # Monitor is not active, re-enable it with desired config
-    hyprctl keyword monitor "$MONITOR,preferred,auto,1"
+    hyprctl keyword monitor "$MONITOR1,preferred,0x0,1"
+    hyprctl keyword monitor "$MONITOR2,preferred,auto,1"
 fi
